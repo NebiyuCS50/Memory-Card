7183157e9659c4c56ece853f902a86272a86d336
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-10-25 13:12:21 +0300

    feat: remove SpeedInsights wrapper from Homepage component

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import Homepage from "./Homepage.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <SpeedInsights projectId="your-project-id">
-      <Homepage />
-    </SpeedInsights>
+    <Homepage />
   </StrictMode>
 );
